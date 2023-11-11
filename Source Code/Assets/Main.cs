06caa6e265d78fa20ad8ddae119771f4f658643c
Eric1K <ericfeng26@gmail.com>
2023-11-11 14:36:02 -0500

    Add files via upload

---
+++ Source Code/Assets/Main.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+    //public Dropdown myDropdown;
+    
+    // 1 = haverford, 2 = brywn mawr, 3 = swat
+    public int college;
+    public GameObject majors;
+	// Use this for initialization
+	void Start () {
+        college = 1;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //int selectedValue = myDropdown.value;
+        //string selectedText = myDropdown.options[selectedValue].text;
+        //Debug.Log("Selected Text: " + selectedText);
+    }
+
+    public void inMajor()
+    {
+        majors.SetActive(false);
+    }
+}
---
+++ Source Code/Assets/dropdown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class dropdown : MonoBehaviour {
+    public Dropdown myDropdown;
+    //public GameObject sprout;
+    public GameObject apple;
+    public GameObject blue;
+    public GameObject tree;
+    //public Button yourButton;
+
+    // Use this for initialization
+    void Start () {
+ 
+    }
+    public void ToggleGameObject()
+    {
+        int selectedValue = myDropdown.value;
+        string selectedText = myDropdown.options[selectedValue].text;
+
+        if (selectedText == "Social Science")
+        {
+            tree.SetActive(!blue.activeSelf);
+        }
+        if (selectedText == "Natural Science")
+        {
+            blue.SetActive(!tree.activeSelf);
+        }
+        if (selectedText == "Humanities")
+        {
+            apple.SetActive(!apple.activeSelf);
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+        
+        
+        // Debug.Log("Selected Text: " + selectedText);
+    }
+}
